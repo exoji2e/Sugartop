c406972d05e5807ca51587f17170b47c94dbd0ad
Måns Magnusson <exoji2e@gmail.com>
2018-09-13 23:32:22 +0200

    add indicator of it beeing a long time since the last reading

--- app/src/main/java/io/exoji2e/erbil/RecentActivity.kt
+++ app/src/main/java/io/exoji2e/erbil/RecentActivity.kt
@@ -22,8 +22,6 @@ class RecentActivity : ErbilActivity() {
             val last = readings.last().value
             val timeStamp = dc.lastTimeStamp()
             val (left, unit) = Time.timeLeft(timeStamp)
-            TimeLeftData.text = left
-            TimeLeftUnit.text = unit
 
             val diff = RawParser.sensor2mmol(predict.value) - RawParser.sensor2mmol(last)
             val trend = if (diff > .5) "↑↑" else if (diff > .25) "↑" else if (diff < -.5) "↓↓" else if (diff < -.25) "↓" else "→"
@@ -36,7 +34,12 @@ class RecentActivity : ErbilActivity() {
                 graph.post { Push2Plot._setPlot(readings.map { g -> g.toReading() } + predict, manual, graph, Time.now() - Time.HOUR * 8, Time.now() + 5 * Time.MINUTE) }
                 ingData.text = Compute.inGoal(4.0, 8.0, readings)
                 avgData.text = String.format("%.1f", avg)
-                recentData.text = String.format("%.1f %s", RawParser.sensor2mmol(predict.value), trend)
+                if (Time.now() - readings.last().utcTimeStamp < Time.HOUR)
+                    recentData.text = String.format("%.1f %s", RawParser.sensor2mmol(predict.value), trend)
+                else
+                    recentData.text = "-"
+                TimeLeftData.text = left
+                TimeLeftUnit.text = unit
             }
         }
         DbWorkerThread.getInstance().postTask(task)
